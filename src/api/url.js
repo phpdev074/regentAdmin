01517a19b0d764f2b41ec5f07334343700a5f0d4
developer2 <kashish@castledigital.com.au>
2024-12-20 18:24:52 +0530

    initial commit

---
+++ src/api/url.js
@@ -0,0 +1,18 @@
+export const BASE_URL = "https://1sg5b0hc-7800.inc1.devtunnels.ms/api/admin/";
+export const IMAGE_BASE_URL = "https://1sg5b0hc-7800.inc1.devtunnels.ms/api/business/";
+
+export const POST_METHOD = "POST";
+export const GET_METHOD = "GET";
+export const PUTMETHOD = "PUT";
+export const DELETE = "DELETE";
+export const UPLOAD_IMAGE ="upload-image?type=images"
+export const AVAILABLE_DAYS = "availableDays";
+export const LOGIN = "login";
+export const GET_PROFILE = "getProfile";
+export const UPDATE_PROFILE = "updateProfile";
+export const GET_USER_LIST = "getUserList";
+export const DELETE_USER = "deleteUser";
+
+
+
+
